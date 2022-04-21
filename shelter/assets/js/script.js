c28700fe8e7a29d675a48c9fb801dbadc97914d3
Alexander <mas@webernetic.by>
2022-04-21 16:48:15 +0300

    feet: add slider

--- shelter/assets/js/script.js
+++ shelter/assets/js/script.js
@@ -19,11 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 	}
 
-	document.addEventListener('click', e => {
-		if(e.target.classList.contains('overflow')) {
-			closeNav();
-		}
-	})
+	
 
 	let amountSlides = '';
 	if(document.documentElement.clientWidth > 1279) {
@@ -87,7 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			
 			arr.forEach(item => {
 				block.insertAdjacentHTML(where, `
-				<div class="item">
+				<div class="item" data-item="${item.name}">
 					<img src="${item.img}" alt="${item.type} ${item.name}" class="item__img">
 					<div class="item__title">${item.name}</div>
 					<button class="item__button btn btn_tr" data-item="${item.name}">Learn more</button>
@@ -119,6 +115,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		if(arrowNext && arrowPrev) {
 			arrowNext.addEventListener('click', e => {
 				visibleSlides = nextSlides;
+				arrowNext.style.pointerEvents = 'none'
+				arrowPrev.style.pointerEvents = 'none'
 				nextSlides = getSlides(amountSlides, true);
 				petsWrapper.querySelectorAll('.item').forEach(item => {
 					item.style.transition = '.5s';
@@ -130,11 +128,15 @@ document.addEventListener('DOMContentLoaded', () => {
 				})
 				setTimeout(() => {
 					innerSlides(petsWrapper, nextSlides);
-					removeItems(amountSlides)
+					removeItems(amountSlides);
+					arrowNext.style.pointerEvents = 'all'
+					arrowPrev.style.pointerEvents = 'all'
 				}, 500)
 			})
 			arrowPrev.addEventListener('click', e => {
 				visibleSlides = prevSlides;
+				arrowNext.style.pointerEvents = 'none'
+				arrowPrev.style.pointerEvents = 'none'
 				prevSlides = getSlides(amountSlides, true);
 				
 				petsWrapper.querySelectorAll('.item').forEach(item => {
@@ -148,11 +150,75 @@ document.addEventListener('DOMContentLoaded', () => {
 				setTimeout(() => {
 					innerSlides(petsWrapper, prevSlides, 'afterbegin');
 					removeItems(amountSlides, 'end')
+					arrowNext.style.pointerEvents = 'all'
+					arrowPrev.style.pointerEvents = 'all'
 				}, 500)
 			})
 		}
 
 	})
+	function closePopup() {
+		document.body.style.overflow = 'visible';
+		if(document.querySelector('.popup-overflow')) {
+			document.querySelector('.popup-overflow').remove();
+		}
+	}
+	document.addEventListener('click', e => {
+		if(e.target.classList.contains('overflow')) {
+			closeNav();
+		}
+		if(e.target.classList.contains('item') || (e.target.parentElement && e.target.parentElement.classList.contains('item'))) {
+			if(document.querySelector('.popup-overflow')) {
+				document.querySelector('.popup-overflow').remove();
+			}
+			let thisPet = {};
+			petsArr.forEach(item => {
+				if(item.name === e.target.getAttribute('data-item')) {
+					thisPet = item
+				} else if (item.name === e.target.parentElement.getAttribute('data-item')) {
+					thisPet = item
+				}
+ 			})
+			if(thisPet.name) {
 
-	
+				document.body.insertAdjacentHTML('beforeend', `
+					<div class="popup-overflow">
+						<div class="popup">
+							<button class="popup-close">
+								<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd" clip-rule="evenodd" d="M7.42618 6.00003L11.7046 1.72158C12.0985 1.32775 12.0985 0.689213 11.7046 0.295433C11.3108 -0.0984027 10.6723 -0.0984027 10.2785 0.295433L5.99998 4.57394L1.72148 0.295377C1.32765 -0.098459 0.68917 -0.098459 0.295334 0.295377C-0.0984448 0.689213 -0.0984448 1.32775 0.295334 1.72153L4.57383 5.99997L0.295334 10.2785C-0.0984448 10.6723 -0.0984448 11.3108 0.295334 11.7046C0.68917 12.0985 1.32765 12.0985 1.72148 11.7046L5.99998 7.42612L10.2785 11.7046C10.6723 12.0985 11.3108 12.0985 11.7046 11.7046C12.0985 11.3108 12.0985 10.6723 11.7046 10.2785L7.42618 6.00003Z" fill="#292929"/>
+								</svg>
+							</button>
+							<img src="${thisPet.img}" alt="${thisPet.type} ${thisPet.name}" class="popup__img" />
+							<div class="popup-body">
+								<h2 class="popup__title">${thisPet.name}</h2>
+								<p class="popup__subtitle">${thisPet.type} - ${thisPet.breed}</p>
+								<p class="popup__text">${thisPet.description}</p>
+								<ul class="popup__ul">
+									<li><span>Age:</span>${thisPet.age}</li>
+									<li><span>Inoculations:</span>${thisPet.inoculations.join(', ')}</li>
+									<li><span>Diseases:</span>${thisPet.diseases.join(', ')}</li>
+									<li><span>Parasites:</span>${thisPet.parasites.join(', ')}</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				`) 
+			}
+			// document.body.style.overflow = 'hidden';
+
+		} 
+		if(e.target.classList.contains('popup-overflow')) {
+			closePopup();
+		}
+		if(e.target.classList.contains('popup-close')) {
+			closePopup();
+		}
+	})
+	document.addEventListener('keydown', e => {
+		if(e.key ==='Escape') {
+			closePopup()
+		}
+	})
+
 })
